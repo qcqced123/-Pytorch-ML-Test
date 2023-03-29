cf51a0610b045727deec6b39b0d2b89f940f7bcd
Lee <97833081+qcqced123@users.noreply.github.com>
2023-03-29 14:27:53 +0900

    Add Pooling

---
+++ model/Pooling.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+from torch import Tensor
+import torch.nn.functional as F
+
+
+# WeightedLayerPooling: Use Intermediate Layer's Embedding
+class WeightedLayerPooling(nn.Module):
+    def __init__(self, num_hidden_layers, layer_start: int = 4, layer_weights = None):
+        super(WeightedLayerPooling, self).__init__()
+        self.layer_start = layer_start
+        self.num_hidden_layers = num_hidden_layers
+        self.layer_weights = layer_weights if layer_weights is not None \
+            else nn.Parameter(
+                torch.tensor([1] * (num_hidden_layers+1 - layer_start), dtype=torch.float)
+            )
+
+    def forward(self, features) -> Tensor:
+        ft_all_layers = features['all_layer_embeddings']
+
+        all_layer_embedding = torch.stack(ft_all_layers)
+        all_layer_embedding = all_layer_embedding[self.layer_start:, :, :, :]
+
+        weight_factor = self.layer_weights.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand(all_layer_embedding.size())
+        weighted_average = (weight_factor*all_layer_embedding).sum(dim=0) / self.layer_weights.sum()
+
+        features.update({'token_embeddings': weighted_average})
+        return features
+
+
+# Attention pooling
+class AttentionPooling(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        self.attention = nn.Sequential(nn.Linear(in_dim, in_dim),
+                                       nn.LayerNorm(in_dim),
+                                       nn.GELU(),
+                                       nn.Linear(in_dim, 1), )
+
+    def forward(self, last_hidden_state, attention_mask):
+        w = self.attention(last_hidden_state).float()
+        w[attention_mask == 0] = float('-inf')
+        w = torch.softmax(w, 1)
+        attention_embeddings = torch.sum(w * last_hidden_state, dim=1)
+        return attention_embeddings
+
+
+# Mean Pooling
+class MeanPooling(nn.Module):
+    def __init__(self):
+        super(MeanPooling, self).__init__()
+
+    def forward(self, last_hidden_state, attention_mask):
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(last_hidden_state.size()).float()
+        sum_embeddings = torch.sum(last_hidden_state * input_mask_expanded, 1)
+        sum_mask = input_mask_expanded.sum(1)
+        sum_mask = torch.clamp(sum_mask, min=1e-9)
+        mean_embeddings = sum_embeddings / sum_mask
+        return mean_embeddings
+
+
+class ConvPooling(nn.Module):
+    def __init__(self):
--- model/model_utils.py
+++ model/model_utils.py
@@ -2,6 +2,11 @@ from torch import Tensor
 def freeze(module) -> None:
     """
     Freezes module's parameters.
+    freezing embeddings and first 2 layers of encoder
+
+    [Example]
+    1) freeze(model.embeddings)
+    2) freeze(model.encoder.layer[:2])
     """
     for parameter in module.parameters():
         parameter.requires_grad = False
@@ -10,6 +15,9 @@ def freeze(module) -> None:
 def get_freezed_parameters(module) -> list[Tensor]:
     """
     Returns names of freezed parameters of the given module.
+
+    [Example]
+    freezed_parameters = get_freezed_parameters(model)
     """
     freezed_parameters = []
     for name, parameter in module.named_parameters():
